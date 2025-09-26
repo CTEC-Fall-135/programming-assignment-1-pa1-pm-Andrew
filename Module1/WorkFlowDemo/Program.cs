c175979c026559a7ee10b371a842a70945666f65
Andrew <a.sabourin@students.clark.edu>
2025-09-25 20:48:06 -0700

    initial
    It is done.

---
+++ Module1/WorkFlowDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Author: Andrew Sabourin
+Date: 2024-06-20
+Assignment: WorkflowDemo
+ */
+namespace WorkflowDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, mountains!");
+            // Drawing a simple mountain scene using ASCII art
+
+            Console.WriteLine("               /\\");
+            Console.WriteLine("  .  *      * /  \\ ");
+            Console.WriteLine("  .  ^   . * /    \\");
+            Console.WriteLine(" .  / \\   . /      \\");
+            Console.WriteLine("   /   \\.  /        \\");
+            Console.WriteLine(" */     \\ /          \\");
+            Console.WriteLine(" /       \\            \\");
+            Console.WriteLine("/         \\            \\");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("  .     .  *   .   *    .    ");
+            Console.WriteLine("------------------------------");
+
+        }
+    }
+}
